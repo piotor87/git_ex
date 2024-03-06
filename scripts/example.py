dc5494edb05efbf096c45416b526d5e97d7d6c1f
pete <pietro.dellabriottaparolo@gmail.com>
2024-03-06 10:15:27 +0200

    added logging function

--- scripts/example.py
+++ scripts/example.py
@@ -11,13 +11,13 @@ def add_log(script_path=os.path.dirname(os.path.abspath(__file__))):
         o.write('\t'.join(line) + "\n")
     return log_file
 
-# def read_log(log_file):
-#     with open(log_file) as i: last_entry = [elem.strip().split("\t")[0] for elem in i][-1]
-#     old_date = datetime.datetime.fromisoformat(last_entry)
-#     new_date = datetime.datetime.now()
-#     print(f"It literally took me {(new_date - old_date).total_seconds()} seconds to switch branches.")
+def read_log(log_file):
+    with open(log_file) as i: last_entry = [elem.strip().split("\t")[0] for elem in i][-1]
+    old_date = datetime.datetime.fromisoformat(last_entry)
+    new_date = datetime.datetime.now()
+    print(f"It literally took me {(new_date - old_date).total_seconds()} seconds to switch branches.")
     
     
 if __name__ == "__main__":
     log_file = add_log()
-    #read_log(log_file)
+    read_log(log_file)
